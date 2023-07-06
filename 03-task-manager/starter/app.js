d27f11924c3d0966eb4e7baf6be8a8973b64973a
Shashi Sharde <66017964+shashi-sharde@users.noreply.github.com>
2023-07-06 15:11:49 +0530

    changed

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express()
+
+app.get('/hello',(req,res)=>{
+    res.send("Server started at 3000")
+})
+
+const port =3000
+
+app.listen(port,`This app is started on port ${port}....`)
+
+
+
